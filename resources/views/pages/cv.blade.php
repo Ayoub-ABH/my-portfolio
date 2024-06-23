444e6b6f06ec8df18509e530b48ec45694f449ba
Ayoub-ABH <aitayoub1002@gmail.com>
2024-06-23 18:35:06 +0100

    add timeline

--- resources/views/pages/cv.blade.php
+++ resources/views/pages/cv.blade.php
@@ -18,15 +18,31 @@
                     <button>Download</button>
                 </p>
             </div>
+
             <div class="education-journey">
-                <div>
-                    .
-                </div>
-                <div>
-                    
-                </div>
-                <div>
-                    
+                <div class="title">Education</div>
+                <div class="timeline">
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
                 </div>
             </div>
         </div>
