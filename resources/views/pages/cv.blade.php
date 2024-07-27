2dbc9513231734449cf3212b7bdfeaff8915a11c
Ayoub-ABH <aitayoub1002@gmail.com>
2024-07-27 20:59:57 +0100

    cv completed

--- resources/views/pages/cv.blade.php
+++ resources/views/pages/cv.blade.php
@@ -45,6 +45,33 @@
                     </div>
                 </div>
             </div>
+
+            <div class="education-journey">
+                <div class="title">Experience</div>
+                <div class="timeline">
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+                    <div class="education-step">
+                        <h4 class="time">2022 - Present</h4>
+                        <p class="degree-field">Master's Degree in Computer Science </p>
+                        <p class="school">1337 School</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
--- resources/views/pages/realisations.blade.php
+++ resources/views/pages/realisations.blade.php
@@ -12,6 +12,58 @@
 
 
         <div class="realisations-container">
+            <div class="realisation">
+                <div class="head">
+                    <div class="img">
+                        <img src= alt="{{ asset("storage/profile.jpg") }}" alt="projet">
+                    </div>
+                    <div class="title">
+                        Student docs
+                    </div>
+                </div>
+                <div class="body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quo! Nulla consectetur impedit eius, voluptates quibusdam debitis excepturi id labore quae eligendi! Dolorum voluptatum deleniti impedit. Perferendis dicta illo rem.
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="head">
+                    <div class="img">
+                        <img src= alt="{{ asset("storage/profile.jpg") }}" alt="projet">
+                    </div>
+                    <div class="title">
+                        Student docs
+                    </div>
+                </div>
+                <div class="body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quo! Nulla consectetur impedit eius, voluptates quibusdam debitis excepturi id labore quae eligendi! Dolorum voluptatum deleniti impedit. Perferendis dicta illo rem.
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="head">
+                    <div class="img">
+                        <img src= alt="{{ asset("storage/profile.jpg") }}" alt="projet">
+                    </div>
+                    <div class="title">
+                        Student docs
+                    </div>
+                </div>
+                <div class="body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quo! Nulla consectetur impedit eius, voluptates quibusdam debitis excepturi id labore quae eligendi! Dolorum voluptatum deleniti impedit. Perferendis dicta illo rem.
+                </div>
+            </div>
+            <div class="realisation">
+                <div class="head">
+                    <div class="img">
+                        <img src= alt="{{ asset("storage/profile.jpg") }}" alt="projet">
+                    </div>
+                    <div class="title">
+                        Student docs
+                    </div>
+                </div>
+                <div class="body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, quo! Nulla consectetur impedit eius, voluptates quibusdam debitis excepturi id labore quae eligendi! Dolorum voluptatum deleniti impedit. Perferendis dicta illo rem.
+                </div>
+            </div>
         </div>
 
     </div>
